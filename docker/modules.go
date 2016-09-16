a3147023dc24488bd2f5cf7fb5f6c57aa0ddea02
“mervy <sads@live.com>
2016-09-16 10:57:38 +0800

    add

---
+++ docker/modules.go
@@ -0,0 +1,7 @@
+package main
+import (
+  _ "gitlab.com/mervynzhang/logspout-logstash"
+  _ "github.com/gliderlabs/logspout/transports/udp"
+  _ "github.com/gliderlabs/logspout/transports/tcp"
+  )
+
